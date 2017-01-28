8fb14f44f7cfbaf375a0dcea969ccad2dce68044
Sylvester Ranjith Francis <whitefire00700@gmail.com>
2017-01-28 13:07:18 +0530

    add user,update user,deleteuser completed

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var User = require('./public/models/user');
 var defects = require('./public/models/Defects');
 var lineMaster = require('./public/models/LineMaster');
 var lineType = require('./public/models/Linetype');
-var part = require('./public/models/Part');
+var Part = require('./public/models/Part');
 var rework = require('./public/models/Rework');
 
 // call the packages we need
@@ -29,6 +29,20 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;        // set our port
 
+
+// =============================================================================
+
+//create a new instance of all models
+
+var user = new User();
+var defect = new defects();
+var linemaster= new lineMaster();
+var linetype = new lineType();
+var part = new Part();
+var rwk = new rework();
+
+// =============================================================================
+
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
@@ -37,6 +51,7 @@ var router = express.Router();              // get an instance of the express Ro
 router.use(function(req, res, next) {
     // do logging
     console.log('Something is happening.');
+    console.log(req.method, req.url);
     next(); // make sure we go to the next routes and don't stop here
 });
 
@@ -45,35 +60,69 @@ router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });
 });
 
-// more routes for our API will happen here
 
-// on routes that end in /bears
-// ----------------------------------------------------
-router.route('/adduser')
+// create a User (accessed at POST http://localhost:8080/api/user)
 
-// create a User (accessed at POST http://localhost:8080/api/adduser)
-    .post(function(req, res) {
+router.post('/user/adduser',function(req, res) {
+    user.UserId = req.body.UserId;  // set the User ID name (comes from the request)
+    user.LoginId = req.body.LoginId;
+    user.Password = req.body.Password;
+    user.Role = req.body.Role;
+    user.Name = req.body.Name;
+    user.EmployeeID = req.body.EmployeeID;
+    // save the user and check for errors
+    user.save(function(err) {
+        if (err)
+            res.send(err);
+        res.json({ message: 'User created!' });
+    });
 
 
 
-        var user = new User(); // create a new instance of the User model
-        user.UserId = req.body.UserId;  // set the User ID name (comes from the request)
+}); //End of create User Api
+
+//Start of Edit/Update User
+router.put('/user/edituser/:user_id',function(req, res) {
+
+    // use our bear model to find the bear we want
+    User.findById(req.params.user_id, function (err,user ) {
+
+        if (err)
+            res.send(err);
+        user.UserId = req.body.UserId;  // update the User's info
         user.LoginId = req.body.LoginId;
         user.Password = req.body.Password;
         user.Role = req.body.Role;
         user.Name = req.body.Name;
         user.EmployeeID = req.body.EmployeeID;
 
-
-
-        // save the user and check for errors
-        user.save(function(err) {
+        // save the user
+        user.save(function (err) {
             if (err)
                 res.send(err);
-            res.json({ message: 'User created!' });
+
+            res.json({message: 'User updated!'});
         });
 
-    }); //end of add user Api
+    });
+
+}); //End of Edit/Update User
+
+//Start of Delete User
+
+router.delete('/user/deleteuser/:user_id',function(req, res) {
+    User.remove({
+        _id: req.params.user_id
+    }, function(err, user) {
+        if (err)
+            res.send(err);
+
+        res.json({ message: 'Successfully deleted' });
+    });
+});
+
+
+
 
 
 // REGISTER OUR ROUTES -------------------------------
